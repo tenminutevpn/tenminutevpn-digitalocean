9ec63b989ed61b3bc534d44e8cd16746c6b8beab
Andrey Gubarev <andrey@andreygubarev.com>
2024-03-19 14:59:11 +0400

    added skip_s3_checksum to terraform configuration

--- main.tf
+++ main.tf
@@ -11,6 +11,7 @@ terraform {
     skip_credentials_validation = true
     skip_region_validation      = true
     skip_metadata_api_check     = true
+    skip_s3_checksum            = true
   }
 }
 
