30b4885e8847fc6ed77e20cf6587fbfc76ccc030
Andrey Gubarev <andrey@andreygubarev.com>
2024-03-22 16:12:20 +0400

    updated tenminutevpn module and version in terraform config

--- main.tf
+++ main.tf
@@ -4,10 +4,10 @@ terraform {
 
 module "tenminutevpn" {
   source  = "tenminutevpn/tenminutevpn/digitalocean"
-  version = "0.1.7"
+  version = "0.1.8-rc1"
 
   tenminutevpn_region  = "fra1"
-  tenminutevpn_version = "v0.1.6"
+  tenminutevpn_version = "v0.1.8"
 }
 
 output "wireguard" {
