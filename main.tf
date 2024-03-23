e516171350a8fca66aeb90a857c1a4db2e6a5d8c
Andrey Gubarev <andrey@andreygubarev.com>
2024-03-23 15:07:33 +0400

    updated tenminutevpn module version to 0.1.9, added wireguard port and outputs for ipv4 address and ssh private key

--- main.tf
+++ main.tf
@@ -4,13 +4,24 @@ terraform {
 
 module "tenminutevpn" {
   source  = "tenminutevpn/tenminutevpn/digitalocean"
-  version = "0.1.8"
+  version = "0.1.9"
 
   tenminutevpn_region  = "fra1"
-  tenminutevpn_version = "v0.1.8"
+  tenminutevpn_version = "v0.1.9"
+
+  wireguard_port = 51820
 }
 
 output "wireguard" {
   value     = module.tenminutevpn.wireguard
+  sensitive = true
+}
+
+output "ipv4_address" {
+  value = module.tenminutevpn.ipv4_address
+}
+
+output "ssh_private_key" {
+  value     = module.tenminutevpn.ssh_private_key
   sensitive = true
 }
